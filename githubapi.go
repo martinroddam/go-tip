fe822ddd7465f3f12c886995b6b52c62baea2f77
Martin Roddam <martin.roddam@gmail.com>
2018-01-24 15:12:30 +0000

    Tweak

--- githubapi.go
+++ githubapi.go
@@ -69,20 +69,21 @@ func getMostRecentlyMergedPullRequest(gitInfo GitInfo) string {
 
 func applyLabelToPullRequest(prNumber string) {
 	var gitInfo = getGitInfo()
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/issues/"+prNumber, gitInfo.Owner, gitInfo.Repo)
+	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/issues/%s/labels", gitInfo.Owner, gitInfo.Repo, prNumber)
 	fmt.Println(url)
 	client := http.Client{
 		Timeout: time.Second * 2, // Maximum of 2 secs
 	}
 
-	var gitHubEditInfo GitHubEditInfo
-	gitHubEditInfo.labels = append(gitHubEditInfo.labels, label)
-	reqBody, jsonErr := json.Marshal(gitHubEditInfo)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-	fmt.Println("Applying label: \n" + string(reqBody[:]))
-	req, err := http.NewRequest(http.MethodPatch, url, strings.NewReader(string(reqBody[:])))
+	//var gitHubEditInfo GitHubEditInfo
+	//gitHubEditInfo.labels = append(gitHubEditInfo.labels, label)
+	//reqBody, jsonErr := json.Marshal(gitHubEditInfo)
+	//if jsonErr != nil {
+	//	log.Fatal(jsonErr)
+	//}
+	//fmt.Println("Applying label: \n" + string(reqBody[:]))
+	//req, err := http.NewRequest(http.MethodPatch, url, strings.NewReader(string(reqBody[:])))
+	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader("[\"" + label "\"]"))
 	if err != nil {
 		log.Fatal(err)
 	}
